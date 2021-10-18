cd9825cbf0a4ad3c71056facf6b7b37caf841a23
Deborah Thompson <deb246@btinternet.com>
2021-10-18 15:28:27 +0000

    added target = "_blank" to open link in new tab.

--- Carbon Calculator.py
+++ Carbon Calculator.py
@@ -1,5 +1,5 @@
 #Carbon footprint calculator of an individual or family.
-print("Welcome to the Carbon Crushers Carbon Calculator. Please provide the following details");
+print("Welcome to the Carbon Crush Carbon Calculator. Please provide the following details");
 carbon_emission=0.0
 family_members=float(input("Enter family members"))
 food=float(input("if you are a non-vegetarian then input 1"))
@@ -8,17 +8,26 @@ if(food==1):
     carbon_emission += (calories*0.01)*family_members
 else:
     carbon_emission += (calories*0.005)*family_members
+
+
+
 driven_miles=float(input("Enter avg kms driven in a year"))
 if(driven_miles>0):
     carbon_emission += driven_miles*0.01
 else:
     print("enter valid kms driven")
+
+
+
 household_utilities=float(input("Enter number of household utilities \nThey include 1)Electricity \n2)Natural Gas \n3)Fuel \n4)oil"))
 total_expenditures=float(input("Enter total expenditures spent after the utilities"))
 if family_members>0 and household_utilities>0 and total_expenditures>0:
     carbon_emission += (0.12*total_expenditures)*household_utilities
 else:
     print("enter valid details")
+
+
+
 
 Electricity = float(input("Enter the average monthly kilowatts per hour from your electric bill. "))*12
 if Electricity < 0:
